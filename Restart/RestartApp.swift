5fb35036fc09b00edcac1d8ef828f716e031854b
Kenneth Carroll <kennethcarroll@Kenneths-MacBook-Pro.local>
2021-10-30 16:25:12 -0400

    Initial Project Creation

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kenneth Carroll on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
